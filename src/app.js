25dc6816e187a0deec5baa442027b8ce812d9d0a
CarlosDavidAraujo <cdavidav19@gmail.com>
2023-04-10 17:29:09 -0300

    adicionando cors

--- src/app.js
+++ src/app.js
@@ -4,11 +4,12 @@ const sequelize = require('../config/database');
 const aveRoutes = require('./routes/ave');
 const casalRoutes = require('./routes/casal');
 const ninhadaRoutes = require('./routes/ninhada');
+const cors = require('cors');
 
 const PORT = process.env.port || 3000;
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.use(express.json());
 
 app.use(aveRoutes);
